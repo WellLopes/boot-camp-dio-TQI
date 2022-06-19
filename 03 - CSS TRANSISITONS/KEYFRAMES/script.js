ea0c77c612eef1a1238eaba004503ce97c66b1d7
Wellerson Lopes <well@edu.unifor.br>
2022-06-18 23:27:00 -0300

    update files

--- 03 - CSS TRANSISITONS/KEYFRAMES/script.js
+++ 03 - CSS TRANSISITONS/KEYFRAMES/script.js
@@ -1,6 +1,8 @@
 var i = 0,
   text,
-  text = 'Hi, my name is Well Lopes'
+  text2
+text = 'Hi, my name is Well Lopes'
+text2 = "I'am a Front-End Developr"
 
 function typing() {
   if (i < text.length) {
