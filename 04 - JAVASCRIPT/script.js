12da5da019f53621e9e5bb2b97929f2a013989fa
Wellerson Lopes <well@edu.unifor.br>
2022-06-19 15:50:45 -0300

    remove old files

--- 04 - JAVASCRIPT/script.js
+++
@@ -1,9 +0,0 @@
-const PRECO = 5
-let desconto = 0.2
-
-function calc_desconto(PRECO, desconto) {
-  let total = PRECO - PRECO * desconto
-  console.log(`O valor do preço final é R$${total},00`)
-}
-
-calc_desconto(5, 0.2)
