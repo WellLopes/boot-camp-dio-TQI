3ac56adca669912629e63c8444dfa7ade4d1fe73
Wellerson Lopes <well@edu.unifor.br>
2022-06-22 22:41:33 -0300

    Add code propose by instructor

--- 04 - JAVASCRIPT/PALINDROMO/main.js
+++ 04 - JAVASCRIPT/PALINDROMO/main.js
@@ -1,36 +1,7 @@
-function palindromo(phrase) {
-  const FIRST_PHRASE = firstCheck(phrase)
-  const SECOND_PHRASE = seconCheck(phrase)
+function checkPalindromo(string) {
+  if(!string) return "string inexistente"
 
-  let say1 = ''
-
-  for(let i = 0; i < FIRST_PHRASE.length && i < SECOND_PHRASE.length; i++){
-    if(FIRST_PHRASE[i] != SECOND_PHRASE[i]) {
-      say1 = 'não'
-    } 
-  }
-
-  return `Os nomes ${FIRST_PHRASE.join('')} e ${SECOND_PHRASE.join('')} ${say1} são palíndromos`
+  return string.split("").reverse().join("") === string
 }
 
-function firstCheck(phrase) {
-  let firstArrayCheck = []
-
-  for(let i = 0; i < phrase.length; i++) {
-    firstArrayCheck.push(phrase[i])
-  }
-
-  return firstArrayCheck
-}
-
-function seconCheck(phrase) {
-  let secondArrayCheck = []
-
-  for(let i = 0; i< phrase.length; i++) {
-    secondArrayCheck.unshift(phrase[i])
-  }
-
-  return secondArrayCheck
-}
-
-console.log(palindromo('pop'))
+console.log(checkPalindromo("ama"))
