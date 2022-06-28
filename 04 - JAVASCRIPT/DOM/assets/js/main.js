7bd19b324d67f68019454dcf707a596852a7b5ea
Wellerson Lopes <well@edu.unifor.br>
2022-06-27 21:48:08 -0300

    Add a teacher code and learn about clean code and code organization and good practice

--- 04 - JAVASCRIPT/DOM/assets/js/main.js
+++ 04 - JAVASCRIPT/DOM/assets/js/main.js
@@ -1,26 +1,38 @@
+
+function changeMode() {
+  chanceClasses()
+  changeText()
+}
+
+function chanceClasses() {
+  getBody.classList.toggle(darkModeClass)
+  myBtn.classList.toggle(darkModeClass)
+  getFooter.classList.toggle(darkModeClass)
+}
+
+function changeText() {
+  const lightMode = 'Light Mode'
+  const darkMode = 'Dark Mode'
+  
+  if(getBody.classList.contains(darkModeClass)) {
+    myBtn.innerHTML = lightMode
+    getH1.innerHTML = darkMode + " ON"
+
+    return
+  } 
+  
+  myBtn.innerHTML = darkMode
+  getH1.innerHTML = lightMode + " ON"
+
+}
+
+
 // Get my BTN and add event listener
 const myBtn = document.getElementById('mode-selector')
       myBtn.addEventListener('click', changeMode)
 
-const checkDm = document.getElementsByClassName('dark-mode')
-
 // Get my html parts
-const getBody = document.getElementById('body')
+const getBody = document.getElementsByTagName('body')[0]
+const getFooter = document.getElementsByTagName('footer')[0]
 const getH1 = document.getElementById('page-title')
-const getFooter = document.getElementById('footer')
-
-function changeMode() {
-  if(checkDm.length === 0){
-    getBody.classList.add('dark-mode')
-    myBtn.classList.add('dark-mode')
-    getFooter.classList.add('dark-mode')
-    getH1.textContent = 'Dark Mode ON'
-    myBtn.textContent = 'Light Mode'
-  } else {
-    getBody.classList.remove('dark-mode')
-    myBtn.classList.remove('dark-mode')
-    getFooter.classList.remove('dark-mode')
-    getH1.textContent = 'Light Mode ON'
-    myBtn.textContent = 'Dark Mode'
-  }
-}
+const darkModeClass = 'dark-mode'
