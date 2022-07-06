6c035f7f247833f129b0d82bef1bc94eb952d5a7
Wellerson Lopes <well@edu.unifor.br>
2022-07-05 23:01:58 -0300

    Add first React APP

---
+++ 05 - REACTJS/REACT-APP/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function primerioJSX() {
+  return (
+    <div>
+      Well Lopes - First React APP
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {primerioJSX()}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
